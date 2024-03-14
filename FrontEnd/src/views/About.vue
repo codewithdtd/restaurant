60efa3cc6b3464efdd088d47dc0bb07390712779
Dat Do <dtdat201202@gmail.com>
2024-03-14 16:57:18 +0700

    trang chu va trang gioi thieu

---
+++ FrontEnd/src/views/About.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="about">
+        <h1>About JDa Chef</h1>
+        <p>Nhà hàng được mở từ năm 1990, là một trong những nhà hàng Trung Hoa lâu đời nhất tại thành phố. Trải qua bao thăng trầm, nơi đây vẫn giữ nguyên hương vị truyền thống của ẩm thực Trung Hoa, níu chân thực khách bởi những món ăn ngon và không gian ấm cúng.</p>
+
+        <p>
+            Thực đơn của nhà hàng vô cùng phong phú, với hơn 100 món ăn đa dạng, từ các món khai vị thanh đạm, món chính đậm đà cho đến các món tráng miệng ngọt ngào. Mỗi món ăn đều được chế biến tỉ mỉ, cầu kỳ, mang hương vị đặc trưng của từng vùng miền Trung Quốc.
+        </p>
+        <p>
+            Đặc biệt, nhà hàng còn sở hữu đội ngũ đầu bếp giàu kinh nghiệm, luôn tâm huyết mang đến cho thực khách những món ăn ngon nhất. Nguyên liệu được sử dụng đều tươi ngon, đảm bảo chất lượng, góp phần tạo nên hương vị hoàn hảo cho từng món ăn.
+        </p>
+        <p>
+            Nếu bạn đang tìm kiếm một nhà hàng Trung Hoa mang đậm dấu ấn thời gian, với hương vị truyền thống và không gian ấm cúng, thì nhà hàng này chính là điểm đến lý tưởng dành cho bạn.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.about {
+    color: #fff;
+    background: url("../assets/bg.jpg");    
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-align: center;
+    height: 100vh;
+    min-height: fit-content;
+}
+.about h1 {
+    padding-top: 20px;
+    font-family: "Dancing Script", cursive;
+    color: var(--color-main);
+    font-size: 2.7rem;
+}
+
+.about p {
+    width: 60%;
+    margin: auto;
+    color: #ffffff;
+    font-weight: 400;
+    /* background-color: #00000059; */
+    margin-top: 20px;
+    line-height: 1.7;
+}
+.about button {
+    font-size: 1.2rem;
+    border-radius: 30px;
+    margin: 10px 30px;
+}
+</style>
--- FrontEnd/src/views/Home.vue
+++ FrontEnd/src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-    <p>Home</p>
+    <div class="home">
+        <h1>Welcome to JDa Chef</h1>
+        <p>Là nhà hàng lâu đời chúng tôi luôn xem trọng trải nghiệm của khách hàng, ở đây chúng tôi có nhiều món ăn ngon và hấp dẫn xin mời quý khách hàng cũng thưởng thức</p>
+        <router-link to="/menu">
+            <button class="btn btn-outline-warning">Xem Menu</button>
+        </router-link>
+    </div>
 </template>
 
 <script>
@@ -9,4 +15,34 @@ export default {
 </script>
 
 <style>
+.home {
+    color: #fff;
+    height: 100vh;
+    /* background: #373737; */
+    background: url("../assets/bg.jpg");    
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-align: center
+    /* align-items: center; */
+}
+.home h1 {
+    padding-top: 100px;
+    font-family: "Dancing Script", cursive;
+    color: var(--color-main);
+    font-size: 2.7rem;
+}
+
+.home p {
+    width: 60%;
+    margin: auto;
+    color: #ffffff;
+    font-weight: 600;
+    background-color: #00000059;
+    padding: 10px 0;
+}
+.home button {
+    font-size: 1.2rem;
+    border-radius: 30px;
+    padding: 10px 30px;
+}
 </style>
