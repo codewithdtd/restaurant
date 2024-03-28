7dc2ee40d8de1865804e9e546676e83c3c662a49
Dat Do <dtdat201202@gmail.com>
2024-03-28 11:21:39 +0700

    Set up frontend admin

---
+++ FrontEndAdmin/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import Navbar from './components/Navbar.vue';
+import Login from './views/Login.vue';
+export default {
+    components: { Navbar, Login },
+}
+</script>
+
+<template>
+    <div id="app">
+        <!-- <div>
+            <Navbar />
+        </div>
+        <div>
+            <router-view />
+        </div> -->
+        <Login />
+    </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&family=Noto+Sans+HK:wght@100..900&family=Oswald:wght@200&family=Pacifico&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap');
+
+#app {
+    padding: 0;
+    font-family: "Work Sans", sans-serif;
+    background: url(./assets/bg.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding: 0;
+    --color-main: rgb(255, 224, 26);
+    --color-background: rgb(37, 37, 37);
+    display: flex;
+}
+</style>
---
+++ FrontEndAdmin/src/services/api.service.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export default (baseURL) => {
+    const api = axios.create({
+        baseURL,
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            // Kiểm tra xem token có tồn tại trong cookie không
+            // Nếu có, thêm token vào headers
+            ...(Cookies.get('token') && {
+                token: `Bearer ${Cookies.get('token')}`,
+            }),
+        },
+    });
+
+    // Intercept request để thêm token vào headers nếu token thay đổi
+    api.interceptors.request.use(config => {
+        const token = Cookies.get('token');
+        if (token) {
+            config.headers.token = `Bearer ${token}`;
+        }
+        return config;
+    });
+    return api;
+};
